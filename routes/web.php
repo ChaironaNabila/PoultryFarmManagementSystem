1d59babaffdd23c02757c5d1425ea51c57d2ab98
Azzahra Nabila Chairona <azzahranabilachairona@apps.ipb.ac.id>
2024-10-18 00:29:30 +0700

    jquery validation

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\SignController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Sign.in');
 });
 
-Route::get('/', [SignController::class, 'index'])->name('login');
+// Route::get('/', [SignController::class, 'index'])->name('login');
 Route::get('/logout', [SignController::class, 'logout'])->name('logout');
 Route::post('/auth', [SignController::class, 'auth'])->name('auth');
