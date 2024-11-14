dc838bc0bc5e7f880e6ad2bdb076d9c843b36f9b
Azzahra Nabila Chairona <azzahranabilachairona@apps.ipb.ac.id>
2024-11-14 12:58:38 +0700

    api

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Kandang;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role = null)
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        if ($role && $user->role !== $role) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+        return $next($request);
+    }
+}
